e71cce0e65a08052d4efa2bd5b5889202793a042
erinfolamirukayat <erinfolamiayomide@gmail.com>
2025-05-29 15:18:11 -0400

    Updated tailwind file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 export default {content: [
   './index.html',
-  './client/**/*.{js,ts,jsx,tsx}'
+  './src/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
+  './pages/**/*.{js,ts,jsx,tsx}'
 ],}
