1991eb77de6c4e28c0bec14aa0fa9a30316c933f
erinfolamirukayat <erinfolamiayomide@gmail.com>
2025-05-29 14:47:09 -0400

    Updated vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // copy to dist root
+        }
+      ]
+    })
+  ]
+});
