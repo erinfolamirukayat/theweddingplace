f037661fdfab181cbc8fe22860f18168a4a06e58
erinfolamirukayat <erinfolamiayomide@gmail.com>
2025-06-04 13:21:30 -0400

    Updated landing page for mobile

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -102,7 +102,7 @@ const Home = () => {
   return <div className="max-w-6xl mx-auto px-2 sm:px-4">
       {/* Banner */}
       <div
-        className="w-full h-56 sm:h-80 md:h-[28rem] bg-cover bg-center flex items-center justify-center relative rounded-b-lg overflow-hidden mb-8"
+        className="w-full h-56 sm:h-80 md:h-[28rem] bg-cover bg-center flex items-center justify-center relative rounded-b-lg overflow-hidden mb-8 mt-4 py-4 sm:mt-8 sm:py-8"
         style={{ backgroundImage: `url(${BANNER_IMAGE})` }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-40" />
